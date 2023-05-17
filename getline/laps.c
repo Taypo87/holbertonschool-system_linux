8c52b5ffe1a4f3eb7548f501d35d4e8d9b6ef83a
Taypo87 <taylor.poczynek87@gmail.com>
2023-05-17 08:08:45 -0700

    work in progress

--- getline/laps.c
+++ getline/laps.c
@@ -2,15 +2,16 @@
 
 void race_state(int *id, size_t size)
 {
-    static int started[5];
+    static int started[10];
     static int lap = 0;
     static int cars = 0;
+    static int inrace = 0;
     int hasstarted = 0;
 
     for (int i = 0; i < size; i++)
     {
-        int arrlen = sizeof started / sizeof started[0];
-        printf("%d\n", arrlen);
+        int arrlen = 10;
+        //printf("%d\n", arrlen);
         for (int x = 0; x < arrlen; x++)
         {
             //printf("%ls", started);
@@ -23,10 +24,18 @@ void race_state(int *id, size_t size)
         //printf("%ls", started);
         if (hasstarted != 1)
         {
-            //printf("%i", id[i]);
-            started[arrlen - i - 1] = id[i];
-            //printf("%i", started[arrlen - 1]);
-          //  printf("Car %d has joined the race\n", id[i]);
+            //printf("%i\n", id[i]);
+            started[(arrlen - size - i - 1)] = id[i];
+
+            //printf("%i\n", started[(arrlen - size - i - 1)]);
+          printf("Car %d has joined the race\n", started[(arrlen - size - i - 1)]);
         }
     }
+   /* int y = 0;
+    while (y < 10)
+    {
+        printf("%i\n", started[y]);
+        y++;
+    }
+    */
 }
---
+++ getline/laps.h
@@ -0,0 +1,20 @@
+#ifndef LAPS_H
+#define LAPS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	int car;
+    int laps;
+	struct node *next;
+} node_t;
+
+void race_state(int *id, size_t size);
+void displayList(node_t* head);
+node_t* createNode(int car);
+void insertAtEnd(node_t** head, int car);
+int racers(node_t* head, int id);
+void displaylap(node_t** head);
+
+#endif
