ade7b959613d9f7d6d3f1407a78a80d8d4d5e56f
Taypo87 <taylor.poczynek87@gmail.com>
2023-05-17 13:11:35 -0700

    fixed betty issues, refactored

--- getline/laps.c
+++ getline/laps.c
@@ -1,118 +1,100 @@
 #include "laps.h"
-
-void displaylap(node_t* head)
+/**
+* displaylap - displays the lap number for each car in the race
+* @head: the head node to a linked list of cars
+* Return: Void
+*/
+void displaylap(node_t *head)
 {
-    printf("Race state:\n");
-    while (head != NULL)
-    {
-        printf("Car %i [%i laps]\n", head->car, head->laps);
-        head = head->next;
-    }
+	printf("Race state:\n");
+	while (head != NULL)
+	{
+		printf("Car %i [%i laps]\n", head->car, head->laps);
+		head = head->next;
+	}
 }
-
-void sortlist(node_t** head)
+/**
+* createNode - creates a new node for the cars linked list
+* @car: the id of the car being created
+* Return: pointer to the new node
+*/
+node_t *createNode(int car)
 {
-    node_t* sorted = NULL;
-    node_t* current = *head;
+	node_t *newNode = (node_t *)malloc(sizeof(node_t));
 
-    while (current != NULL) {
-        node_t* next = current->next;
-
-        // Insert current node into the sorted list
-        if (sorted == NULL || current->car < sorted->car) {
-            current->next = sorted;
-            sorted = current;
-        }
-        else {
-            node_t* temp = sorted;
-            while (temp->next != NULL && temp->next->car < current->car) {
-                temp = temp->next;
-            }
-            current->next = temp->next;
-            temp->next = current;
-        }
-
-        current = next;
-    }
-
-    *head = sorted;
+	newNode->car = car;
+	newNode->laps = 0;
+	newNode->next = NULL;
+	return (newNode);
 }
+/**
+* insertCarInOrder - adds a new car to the race, and updates cars laps
+* @head: first node in the list of cars
+* @car: id of the car being added or updated
+* Return: pointer to the updated list
+*/
+node_t *insertCarInOrder(node_t *head, int car)
+{
+	node_t *newNode = createNode(car);
 
+	if (head == NULL || car < head->car)
+	{
+		newNode->next = head;
+		return (newNode);
+	}
+
+	node_t *current = head;
+
+	while (current->next != NULL && car > current->next->car)
+	{
+		current = current->next;
+	}
+
+	newNode->next = current->next;
+	current->next = newNode;
+	return (head);
+}
+/**
+* race_state - calls helper functions to create and track cars
+* @id: array containing car numbers
+* @size: number of cars in id
+* Return: Void
+*/
 void race_state(int *id, size_t size)
 {
-    static node_t *head = NULL;
-    int addracer = 0;
-    if (size == 0)
-    {
-        node_t* current = head;
-        while (current != NULL) 
-        {
-            node_t* temp = current;
-            current = current->next;
-            free(temp);
-            return;
-        }
-    }
-    if (head == NULL)
-    {
-        for(int i = 0; i < size; i++)
-        {
-            insertAtEnd(&head, id[i]);
-            printf("Car %i has joined the race!\n", id[i]);
+	static node_t *head;
 
-        }
-    }
-    else
-    {
-        for(int i = 0; i < size; i++)
-        {
-            addracer = racers(head, id[i]);
-            if (addracer == 0)
-            {
-                insertAtEnd(&head, id[i]);
-                printf("Car %i has joined the race!\n", id[i]);
-            }
-        }
-    }
-    sortlist(&head);
-    displaylap(head);
-    //laps++;
-    //printf("laps = %i\n", laps);
-}
+	if (size == 0)
+	{
+		while (head != NULL)
+		{
+			node_t *temp = head;
 
-int racers(node_t* head, int id)
-{
-    while (head != NULL)
-    {
-        if (id == head->car)
-        {
-            head->laps = head->laps + 1;
-            return (1);
-        }
-        head = head->next;
-    }
-    return (0);
-}
-// Function to create a new node
-node_t* createNode(int car)
-{
-    node_t* newNode = (node_t*)malloc(sizeof(node_t));
-    newNode->car = car;
-    newNode->next = NULL;
-    return newNode;
-}
+			head = head->next;
+			free(temp);
+		}
+		return;
+	}
+	for (int i = 0; i < size; i++)
+	{
+		int addracer = 0;
+		node_t *current = head;
 
-void insertAtEnd(node_t** head, int car)
-{
-    node_t* newNode = createNode(car);
-    if (*head == NULL) {
-        *head = newNode;
-        return;
-    }
-    node_t* temp = *head;
-    while (temp->next != NULL) {
-        temp = temp->next;
-    }
-    temp->next = newNode;
+		while (current != NULL)
+		{
+			if (id[i] == current->car)
+			{
+				current->laps++;
+				addracer = 1;
+				break;
+			}
+			current = current->next;
+		}
+		if (!addracer)
+		{
+			head = insertCarInOrder(head, id[i]);
+			printf("Car %i has joined the race!\n", id[i]);
+		}
+	}
+	displaylap(head);
 }
-
--- getline/laps.h
+++ getline/laps.h
@@ -6,15 +6,14 @@
 
 typedef struct node {
 	int car;
-    int laps;
+	int laps;
 	struct node *next;
 } node_t;
 
 void race_state(int *id, size_t size);
 node_t* createNode(int car);
-void insertAtEnd(node_t** head, int car);
-int racers(node_t* head, int id);
-void displaylap(node_t** head);
-void sortlist(node_t** head);
+node_t *insertCarInOrder(node_t *head, int car);
+void displaylap(node_t* head);
+
 
 #endif
