03f2372f6c85841a64762cd0e37774416d53fe54
Taypo87 <taylor.poczynek87@gmail.com>
2023-05-17 13:16:52 -0700

    fixed compiler error

--- getline/laps.c
+++ getline/laps.c
@@ -75,7 +75,8 @@ void race_state(int *id, size_t size)
 		}
 		return;
 	}
-	for (size_t i = 0; i < size; i++)
+	size_t i;
+	for (i = 0; i < size; i++)
 	{
 		int addracer = 0;
 		node_t *current = head;
