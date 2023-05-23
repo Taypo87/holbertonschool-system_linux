2d3d0f2b3ea3a88f1e26dfdc4313950f3570041b
Taypo87 <taylor.poczynek87@gmail.com>
2023-05-23 07:27:36 -0700

    fixed compiler errors

--- getline/_getline.c
+++ getline/_getline.c
@@ -9,12 +9,12 @@ char* _getline(const int fd)
 {
     static char* leftovers = NULL;
     static size_t leftovercount = 0;
+    char* temp;
 
     char buffer[READ_SIZE];
-    ssize_t bytesread;
+    size_t bytesread;
     size_t i, j;
 
-    // Check if there are leftovers from previous read
     if (leftovers != NULL)
     {
         for (i = 0; i < leftovercount; i++)
@@ -23,7 +23,6 @@ char* _getline(const int fd)
                 break;
         }
 
-        // If a newline is found in leftovers, extract the line
         if (i < leftovercount)
         {
             char* line = malloc((i + 1) * sizeof(char));
@@ -39,8 +38,7 @@ char* _getline(const int fd)
         }
         else
         {
-            // No newline found, save leftovers for next iteration
-            char* temp = realloc(leftovers, (leftovercount + READ_SIZE) * sizeof(char));
+            temp = realloc(leftovers, (leftovercount + READ_SIZE) * sizeof(char));
             if (temp == NULL)
             {
                 fprintf(stderr, "Error: Memory allocation failed\n");
@@ -52,7 +50,6 @@ char* _getline(const int fd)
     }
     else
     {
-        // Allocate memory for leftovers
         leftovers = malloc(READ_SIZE * sizeof(char));
         if (leftovers == NULL)
         {
@@ -61,7 +58,6 @@ char* _getline(const int fd)
         }
     }
 
-    // Read from file and search for newline
     while ((bytesread = read(fd, buffer, READ_SIZE)) > 0)
     {
         for (i = 0; i < bytesread; i++)
@@ -76,17 +72,14 @@ char* _getline(const int fd)
                     return (NULL);
                 }
 
-                // Copy leftovers
                 for (j = 0; j < leftovercount; j++)
                     line[j] = leftovers[j];
 
-                // Copy current line
                 for (j = 0; j < i; j++)
                     line[leftovercount + j] = buffer[j];
 
                 line[leftovercount + i] = '\0';
 
-                // Save remaining characters as leftovers
                 leftovercount = bytesread - i - 1;
                 for (j = 0; j < leftovercount; j++)
                     leftovers[j] = buffer[i + 1 + j];
@@ -95,7 +88,6 @@ char* _getline(const int fd)
             }
         }
 
-        // No newline found, expand leftovers if necessary
         char* temp = realloc(leftovers, (leftovercount + bytesread) * sizeof(char));
         if (temp == NULL)
         {
@@ -105,14 +97,12 @@ char* _getline(const int fd)
         }
         leftovers = temp;
 
-        // Copy buffer to leftovers
         for (i = 0; i < bytesread; i++)
             leftovers[leftovercount + i] = buffer[i];
 
         leftovercount += bytesread;
     }
 
-    // Reached end of file, free memory and return NULL
     free(leftovers);
     leftovers = NULL;
     return (NULL);
