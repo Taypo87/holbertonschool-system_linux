ce93fbfef212211d759bd0b04cd88e54f67ff48e
Taypo87 <taylor.poczynek87@gmail.com>
2023-05-27 13:26:37 -0700

    task 0 - 1

---
+++ ls/concept.c
@@ -0,0 +1,39 @@
+#include "concept.h"
+
+void main (int argc, char **argv)
+{
+    DIR *dir;
+
+    if (argc < 2)
+    {
+        dir = opendir(".");
+        if (dir == NULL)
+        {
+            printf("directoy open failed");
+            return;
+        }
+        struct dirent *entity;
+        entity = readdir(dir);
+        while (entity != NULL)
+        {
+            printf("%s\n", entity->d_name);
+            entity = readdir(dir);
+        }
+    }
+    else
+    {
+        dir = opendir(argv[1]);
+        if (dir == NULL)
+        {
+            printf("directoy open failed");
+            return;
+        }
+        struct dirent *entity;
+        entity = readdir(dir);
+        while (entity != NULL)
+        {
+            printf("%s\n", entity->d_name);
+            entity = readdir(dir);
+        }
+    }
+}
---
+++ ls/concept.h
@@ -0,0 +1,11 @@
+#ifndef _CONCEPT_
+#define _CONCEPT_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+
+#endif
