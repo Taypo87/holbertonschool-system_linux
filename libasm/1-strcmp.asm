27f9fcd8b8a313b6eee94ee14cd027c0e9dd8a4e
Taypo87 <taylor.poczynek87@gmail.com>
2023-07-18 10:49:04 +0000

    task 1

---
+++ libasm/1-strcmp.asm
@@ -0,0 +1,32 @@
+BITS 64
+
+section .text
+global strcmp
+
+strcmp:
+    mov rsi, rdi
+    mov rdx, r8
+
+    cmp rdi, rsi
+    je .end
+
+.loop:
+    mov al, [rdi]
+    cmp al, [rsi]
+    jne .unequal
+
+    cmp al, 0
+    je .end
+
+    inc rdi
+    inc rsi
+    dec rdx
+    jnz .loop
+
+.end:
+    mov eax, 0
+    ret
+
+.unequal:
+    mov eax, 1
+    ret
