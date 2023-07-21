941b04feb1ccc5f04f351ef4374966bf7ac69c47
Taypo87 <taylor.poczynek87@gmail.com>
2023-07-21 15:11:00 +0000

    trying things

--- signals/0-handle_signal.c
+++ signals/0-handle_signal.c
@@ -1,25 +1,26 @@
 #include "signals.h"
 
-int handle_signal(void)
 /*
 * handle_signal - catches the signal and calls handle_sigint
 * Return: -1 on error 0 on success
 */
+int handle_signal(void)
 {
-    if (signal(SIGINT, handle_sigint) == SIG_ERR)
-    {
-        return (-1);
-    }
-    else
-    {
-        return (0);
-    }
+	if (signal(SIGINT, handle_sigint) == SIG_ERR)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 /*
-* handle_signal - catches the signal and calls handle_sigint
+* handle_sigint - handles sigint
 * @sig: signal
 */
 void handle_sigint(int sig)
 {
-    printf("Gotcha! [%i]\n", sig);
+	printf("Gotcha! [%i]\n", sig);
+	fflush(stdout);
 }
