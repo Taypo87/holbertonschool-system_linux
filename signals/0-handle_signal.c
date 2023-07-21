6ac661d44a1d7907f1ddd5dbaca8a21019ad217e
Taypo87 <taylor.poczynek87@gmail.com>
2023-07-21 14:55:38 +0000

    added newline

--- signals/0-handle_signal.c
+++ signals/0-handle_signal.c
@@ -1,6 +1,10 @@
 #include "signals.h"
 
 int handle_signal(void)
+/*
+* handle_signal - catches the signal and calls handle_sigint
+* Return: -1 on error 0 on success
+*/
 {
     if (signal(SIGINT, handle_sigint) == SIG_ERR)
     {
@@ -11,7 +15,10 @@ int handle_signal(void)
         return (0);
     }
 }
-
+/*
+* handle_signal - catches the signal and calls handle_sigint
+* @sig: signal
+*/
 void handle_sigint(int sig)
 {
     printf("Gotcha! [%i]\n", sig);
