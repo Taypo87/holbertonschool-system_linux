37c2ad795f7cb9509bb5a005755f9c5b221a3ddc
Taypo87 <taylor.poczynek87@gmail.com>
2023-07-22 11:21:01 +0000

    trying stuff

--- signals/1-current_handler_signal.c
+++ signals/1-current_handler_signal.c
@@ -3,6 +3,8 @@
 void (*current_handler_signal(void))(int)
 {
     void (*handler)(int) = signal(SIGINT, SIG_DFL);
+    if (signal(SIGINT, SIG_DFL) == SIG_ERR)
+        return NULL;
     
     return ((signal(SIGINT, handler)));
 }
