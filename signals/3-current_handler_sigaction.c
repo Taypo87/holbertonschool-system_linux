9fe4e751a179cc9abf80fe887752c6d0a66b3917
Taypo87 <taylor.poczynek87@gmail.com>
2023-07-22 13:21:17 +0000

    task 3

--- signals/3-current_handler_sigaction.c
+++ signals/3-current_handler_sigaction.c
@@ -2,7 +2,7 @@
 
 /**
 * current_handler_sigaction - retireves the current handler of SIGINT
-* Return: a pointer to the current handler 
+* Return: a pointer to the current handler
 *
 */
 
@@ -10,7 +10,8 @@ void (*current_handler_sigaction(void))(int)
 {
 	struct sigaction sa;
 
-	(sigaction(SIGINT, NULL, &sa));
+	if (sigaction(SIGINT, NULL, &sa)) == SIG_ERR;
+		return (NULL);
 
 	return (sa.sa_handler);
 }
