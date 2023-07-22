5798dc4586d56a047c38979d9fbbd5568f714ad0
Taypo87 <taylor.poczynek87@gmail.com>
2023-07-22 13:05:06 +0000

    updated prototypes

--- signals/signals.h
+++ signals/signals.h
@@ -10,6 +10,7 @@ int handle_signal(void);
 void handle_sigint(int sig);
 void (*current_handler_signal(void))(int);
 int handle_sigaction(void);
+void sigaction_handler(int sig);
 void (*current_handler_sigaction(void))(int);
 int trace_signal_sender(void);
 
