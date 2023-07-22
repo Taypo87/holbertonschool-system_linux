8551ae4de7af8f726987cdec24d546fbe22ec4e6
Taypo87 <taylor.poczynek87@gmail.com>
2023-07-22 12:43:03 +0000

    betty

--- signals/1-current_handler_signal.c
+++ signals/1-current_handler_signal.c
@@ -1,9 +1,14 @@
 #include "signals.h"
 
+/**
+* current_handler_signal - retireves the current handler for SIGINT
+* Return: pointer to the current handler
+*
+*/
 void (*current_handler_signal(void))(int)
 {
-    void (*func)(int) = signal(SIGINT, SIG_DFL);
-    
-    signal(SIGINT, func);
-    return (func);
+	void (*func)(int) = signal(SIGINT, SIG_DFL);
+
+	signal(SIGINT, func);
+	return (func);
 }
