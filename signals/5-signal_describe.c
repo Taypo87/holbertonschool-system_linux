7ef33fa4561119ac98ff5bc59e3ac25b920e3107
Taypo87 <taylor.poczynek87@gmail.com>
2023-07-23 08:01:52 +0000

    task 5

---
+++ signals/5-signal_describe.c
@@ -0,0 +1,18 @@
+#include "signals.h"
+
+int main(int argc, char **argv)
+{
+    int number;
+
+    if (argc != 2)
+        {
+            printf("Usage: %s <signum>", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    number = atoi(argv[1]);
+    if (number >= 0 && number < NSIG)
+        printf("%i: %s\n", argv[1], sys_siglist[number]);
+    else
+        printf("%i: Unknown signal %i", argv[1], argv[1]);
+    return (EXIT_SUCCESS);
+}
