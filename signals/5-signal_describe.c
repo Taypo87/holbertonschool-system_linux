5277bb6207b772288241520aea90ff0d08d7cdfa
Taypo87 <taylor.poczynek87@gmail.com>
2023-07-23 08:10:04 +0000

    task 5

--- signals/5-signal_describe.c
+++ signals/5-signal_describe.c
@@ -10,10 +10,10 @@ int main(int argc, char **argv)
 	int number;
 
 	if (argc != 2)
-		{
-			printf("Usage: %s <signum>", argv[0]);
-			exit(EXIT_FAILURE);
-		}
+	{
+		printf("Usage: %s <signum>", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 	number = atoi(argv[1]);
 	if (number > 0 && number < NSIG)
 		printf("%i: %s\n", number, sys_siglist[number]);
