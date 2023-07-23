99c10be719530a1a570fd10b72e32a348695cdc8
Taypo87 <taylor.poczynek87@gmail.com>
2023-07-23 08:07:59 +0000

    task 5

--- signals/5-signal_describe.c
+++ signals/5-signal_describe.c
@@ -1,18 +1,23 @@
 #include "signals.h"
-
+/**
+* main - checks if signal number is in range and prints the corresponding signal
+* @argc: the argument count
+* @argv: the argument array
+* Return - EXIT_FAILURE EXIT _SUCCESS
+*/
 int main(int argc, char **argv)
 {
-    int number;
+	int number;
 
-    if (argc != 2)
-        {
-            printf("Usage: %s <signum>", argv[0]);
-            exit(EXIT_FAILURE);
-        }
-    number = atoi(argv[1]);
-    if (number > 0 && number < NSIG)
-        printf("%i: %s\n", number, sys_siglist[number]);
-    else
-        printf("%i: Unknown signal %i\n", number, number);
-    return (EXIT_SUCCESS);
+	if (argc != 2)
+		{
+			printf("Usage: %s <signum>", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	number = atoi(argv[1]);
+	if (number > 0 && number < NSIG)
+		printf("%i: %s\n", number, sys_siglist[number]);
+	else
+		printf("%i: Unknown signal %i\n", number, number);
+	return (EXIT_SUCCESS);
 }
