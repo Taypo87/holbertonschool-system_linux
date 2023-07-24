0acd28217aa93f760e0d9735df3f5676e0dcdeaa
Taypo87 <taylor.poczynek87@gmail.com>
2023-07-24 09:54:59 +0000

    updated prototypes

--- signals/signals.h
+++ signals/signals.h
@@ -14,7 +14,6 @@ void sigaction_handler(int sig);
 void (*current_handler_sigaction(void))(int);
 void trace_handler(int sig, siginfo_t *siginfo, void *context);
 int trace_signal_sender(void);
-
-
+void handle_sigint_once(int sig);
 
 #endif
