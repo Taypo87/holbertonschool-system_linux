785e399ddfda881857ef8fc17259ab2ae59e84cf
Taypo87 <taylor.poczynek87@gmail.com>
2023-07-31 12:46:49 +0000

    looking for segfault

--- nm_objdump/0-parser.c
+++ nm_objdump/0-parser.c
@@ -10,6 +10,7 @@ int parse_args(int argc, char **argv)
     if (argc == 1)
     {
         fd = open("a.out", O_RDONLY);
+        printf("break 10");
         if (!fd)
         {
             printf("%s: 'a.out': No such file\n", argv[0]);
@@ -22,7 +23,9 @@ int parse_args(int argc, char **argv)
     {
         for (i = 1; i < argc; i++)
         {
+            printf("this should print");
             fd = open(argv[i], O_RDONLY);
+            printf("break 11");
             if (!fd)
             {
                 printf("%s: '%s': No such file\n", argv[0], argv[i]);
@@ -30,7 +33,10 @@ int parse_args(int argc, char **argv)
             else
             {
                 if (stat(argv[i], &st) == 0 && st.st_mode & S_IXUSR)
+                {
+                    printf("break 12");
                     nm_wrapper(fd);
+                }
                 else
                     printf("%s: %s: File format not recognized\n", argv[0], argv[1]);
             }
