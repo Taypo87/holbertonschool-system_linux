87bb512290447088dbb3c242b4325e4003cfc97b
Taypo87 <taylor.poczynek87@gmail.com>
2023-08-05 12:34:53 +0000

    betty checks

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -1,27 +1,33 @@
 #include "cpython.h"
-
+void
+/**
+* print_python_list - prints info about a given python list
+* @p: the pointer to the python list object
+* Return: void
+*/
 void print_python_list(PyObject *p)
 {
-    Py_ssize_t len, i = 0;
-    PyTypeObject *type;
-    PyObject *element;
-    PyListObject *list;
+	Py_ssize_t len, i = 0;
+	PyTypeObject *type;
+	PyObject *element;
+	PyListObject *list;
 
-    // looks like we need size of python list,  allocated, and the elements of the list and their type
-    if (PyList_Check(p) == 1)
-    {
-        len = PyList_Size(p);
-        list = (PyListObject *)p;
-        
+	// looks like we need size of python list,  allocated, and the elements of the list and their type
+	if (PyList_Check(p) == 1)
+	{
+		len = PyList_Size(p);
+		list = (PyListObject *)p;
+		
 
-        printf("[*] Python list info\n");
-        printf("[*] Size of the Python List = %d\n", (int)len);
-        printf("[*] Allocated = %d\n", (int)list->allocated);
-        for (i = 0; i < len; i++)
-        {
-             element = PyList_GetItem(p, i);
-             type = Py_TYPE(element);
-             printf("Element %d: %s\n", ((int)i), type->tp_name);
-        }
-    }
+		printf("[*] Python list info\n");
+		printf("[*] Size of the Python List = %d\n", (int)len);
+		printf("[*] Allocated = %d\n", (int)list->allocated);
+		for (i = 0; i < len; i++)
+		{
+			 element = PyList_GetItem(p, i);
+			 type = Py_TYPE(element);
+			 printf("Element %d: %s\n", ((int)i), type->tp_name);
+		}
+	}
 }
+	
