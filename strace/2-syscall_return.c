c4b3ef111c9098a3ce2f29617252c2dbc3bea73f
Taypo87 <taylor.poczynek87@gmail.com>
2023-09-08 19:04:55 +0000

    reowrked

--- strace/2-syscall_return.c
+++ strace/2-syscall_return.c
@@ -1,10 +1,10 @@
 #include "syscall_header.h"
 #include "syscalls.h"
 
-int main(int argc, char** argv)
+int main(int argc, char** argv, char** envp)
 {
         pid_t pid;
-        int status, flip;
+        int status, flip, count = 1;
         struct user_regs_struct regs;
 
         if (check_arg(argc, argv) == 1)
@@ -15,35 +15,35 @@ int main(int argc, char** argv)
 
         if (pid == 0)
         {
-            ptrace(PTRACE_TRACEME, pid, 0, 0);
+            ptrace(PTRACE_TRACEME, 0, 0, 0);
             kill(getpid(), SIGSTOP);
-            execvp(argv[1], argv+1);
+            execve(argv[1], argv+1, envp);
             exit(EXIT_FAILURE);
         }
         else
         {
-            waitpid(pid, &status, 0);
+            wait(&status);
             ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACESYSGOOD);
-            ptrace(PTRACE_SYSCALL, pid, 0, 0);
-            waitpid(pid, &status, 0);
-            ptrace(PTRACE_SYSCALL, pid, 0, 0);
-            for (flip = 1; !WIFEXITED(status); flip ^= 1)
+            //ptrace(PTRACE_SYSCALL, pid, 0, 0);
+            //wait(&status);
+            for (flip = 0; !WIFEXITED(status); flip ^= 1)
             {
                 memset(&regs, 0, sizeof(regs));
                 ptrace(PTRACE_GETREGS, pid, 0, &regs);
                 if ((int)regs.orig_rax == 252)
                     break;
-                if (!flip)
+                if (!flip && (count > 2))
                 {
                     printf("%s", syscalls_64_g[regs.orig_rax].name);
-                }
-                if (flip)
-                {
                     fflush(stdout);
+                }
+                if (flip && (count > 2))
+                {
                     printf(" = %s%lx\n", regs.rax?"0x":"", (long)regs.rax);
                 }
                 ptrace(PTRACE_SYSCALL, pid, 0, 0);
-                waitpid(pid, &status, 0);
+                wait(&status);
+                count++;
             }
         }
         printf(" = ?\n");
