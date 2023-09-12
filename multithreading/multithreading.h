21c17a860777ae4c36fc1e332f7819d34239e33f
Taypo87 <taylor.poczynek87@gmail.com>
2023-09-12 07:03:22 +0000

    removed an uneaded prototype

--- multithreading/multithreading.h
+++ multithreading/multithreading.h
@@ -72,4 +72,3 @@ void *thread_entry(void *arg);
 int tprintf(char const *format, ...);
 void blur_portion(blur_portion_t const *portion);
 void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
-list_t *prime_factors(char const *s);
