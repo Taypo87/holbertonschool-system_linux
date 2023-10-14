02dd844e9d342757bb9723c25634ac01c9f1ed2d
Taypo87 <taylor.poczynek87@gmail.com>
2023-10-14 03:34:53 -0500

    had the directory in the wrong repository

---
+++ sockets/socket.h
@@ -0,0 +1,22 @@
+#ifndef _SOCKET_
+#define _SOCKET_
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+
+
+
+
+
+
+
+
+
+
+#endif
