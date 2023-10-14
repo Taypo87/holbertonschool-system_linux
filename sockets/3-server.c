a8a91eb13d18b1c25dcc88f7aeef0345c8640144
Taypo87 <taylor.poczynek87@gmail.com>
2023-10-14 18:15:03 -0500

    task 3

---
+++ sockets/3-server.c
@@ -0,0 +1,37 @@
+#include "socket.h"
+/**
+ * main - sets up a listening socket and accepts connections
+ * Return: 0 on success
+*/
+int main(void)
+{
+	int socketfd, clientfd, backlog = 8;
+	struct sockaddr_in *address, *inbound_in;
+	struct sockaddr *inbound_address;
+	socklen_t *inbound_addrlength = 0;
+    char message[40];
+    ssize_t byte_recieved;
+
+	socketfd = socket(AF_INET, SOCK_STREAM, 0);
+	address = calloc(1, sizeof(struct sockaddr_in));
+	inbound_address = calloc(1, sizeof(struct sockaddr));
+	address->sin_family = AF_INET;
+	address->sin_port = htons(12345);
+	address->sin_addr.s_addr = INADDR_ANY;
+	bind(socketfd, (struct sockaddr *)address, sizeof(struct sockaddr_in));
+	printf("Server listening on port 12345\n");
+	while (listen(socketfd, backlog) == 0)
+	{
+		clientfd = accept(socketfd, inbound_address, inbound_addrlength);
+		inbound_in = (struct sockaddr_in *)inbound_address;
+		printf("Client connected: %s\n", inet_ntoa(inbound_in->sin_addr));
+        byte_recieved = recv(clientfd, message, sizeof(message), 0);
+        if (byte_recieved < 0)
+        {
+            message[byte_recieved] = '\0';
+            printf("Message received: %s\n", message);
+        }
+		break;
+	}
+	return (0);
+}
