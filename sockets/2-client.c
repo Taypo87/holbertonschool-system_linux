ba1a3a9a12660661d43d584279cdffa56bd752b0
Taypo87 <taylor.poczynek87@gmail.com>
2023-10-14 17:18:50 -0500

    task 2

--- sockets/2-client.c
+++ sockets/2-client.c
@@ -3,8 +3,9 @@
 int main(int argc, char **argv)
 {
 	int socketfd;
-	struct sockaddr_in *addr;
-	socklen_t addr_size = 1;
+	struct sockaddr_in *addr = calloc(1, sizeof(struct sockaddr_in));
+	socklen_t addr_size = sizeof(struct sockaddr_in);
+	char *local = "localhost";
 
 	if (argc != 3)
 	{
@@ -12,15 +13,20 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 	socketfd = socket(AF_INET, SOCK_STREAM, 0);
+	addr->sin_family = AF_INET;
 	addr->sin_port = htons(atoi(argv[2]));
-	inet_pton(AF_INET, argv[1], &(addr->sin_addr));
-	if (connect(socketfd, addr, addr_size) == 0)
+	if (strcmp(local, argv[1]) == 0)
+		inet_pton(AF_INET, "127.0.0.1", &addr->sin_addr);
+	else
+		inet_pton(AF_INET, argv[1], &(addr->sin_addr));
+	if (connect(socketfd, (struct sockaddr *)addr, addr_size) == 0)
 	{
-		printf("Connected to %s:%s", argv[1], argv[2]);
+		printf("Connected to %s:%s\n", argv[1], argv[2]);
 		exit(EXIT_SUCCESS);
 	}
 	else
 	{
+		printf("ERROR\n");
 		exit(EXIT_FAILURE);
 	}
 
