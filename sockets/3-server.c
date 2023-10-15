d768d8a70c2182fb9d41bef0e3333d5a62d533c3
Taypo87 <taylor.poczynek87@gmail.com>
2023-10-15 05:30:04 -0500

    fixed inbound address length

--- sockets/3-server.c
+++ sockets/3-server.c
@@ -8,9 +8,9 @@ int main(void)
 	int socketfd, clientfd, backlog = 8;
 	struct sockaddr_in *address, *inbound_in;
 	struct sockaddr *inbound_address;
-	socklen_t *inbound_addrlength = 0;
-    char message[40];
-    ssize_t byte_recieved;
+	socklen_t *inbound_addrlength;
+    char message[1024];
+    ssize_t byte_received;
 
 	socketfd = socket(AF_INET, SOCK_STREAM, 0);
 	address = calloc(1, sizeof(struct sockaddr_in));
@@ -18,6 +18,7 @@ int main(void)
 	address->sin_family = AF_INET;
 	address->sin_port = htons(12345);
 	address->sin_addr.s_addr = INADDR_ANY;
+    *inbound_addrlength = (socklen_t)sizeof(struct sockaddr);
 	bind(socketfd, (struct sockaddr *)address, sizeof(struct sockaddr_in));
 	printf("Server listening on port 12345\n");
 	while (listen(socketfd, backlog) == 0)
@@ -25,10 +26,10 @@ int main(void)
 		clientfd = accept(socketfd, inbound_address, inbound_addrlength);
 		inbound_in = (struct sockaddr_in *)inbound_address;
 		printf("Client connected: %s\n", inet_ntoa(inbound_in->sin_addr));
-        byte_recieved = recv(clientfd, message, sizeof(message), 0);
-        if (byte_recieved < 0)
+        byte_received = recv(clientfd, message, sizeof(message), 0);
+        if (byte_received > 0)
         {
-            message[byte_recieved] = '\0';
+            message[byte_received] = '\0';
             printf("Message received: %s\n", message);
         }
 		break;
